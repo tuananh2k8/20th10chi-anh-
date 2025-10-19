4d4c39bc11ac2f6c7d5d473fe7a2a105c6e96668
quaithuk8 <tuananh242k8@gmail.com>
2025-10-19 19:20:58 +0700

    anhchioi

--- style/script.js
+++ style/script.js
@@ -32,7 +32,7 @@ book.appendChild(introPage);
 pages.push(introPage);
 
 const images = [];
-for (let i = 1; i <= 30; i++) { //Chỉnh số lượng ảnh ở đây
+for (let i = 1; i <= 57; i++) { //Chỉnh số lượng ảnh ở đây
   images.push(`./style/image/Anh (${i}).jpg`);
 }
 
